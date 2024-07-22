fd23c713ba4554d4fd512cd37124f02813394202
David Fulmer <dfulmer@umich.edu>
2024-07-22 09:34:04 -0400

    feat: adding sixth script to directory 5

---
+++ 5/maker_original6.py
@@ -0,0 +1,34 @@
+import glob
+import pandas as pd
+from openpyxl import Workbook
+
+# Create a new Excel Workbook
+wb = Workbook()
+
+# Save it with the name 'SEEES search request'
+wb.save('SEEES search request.xlsx')
+
+# Get a list of all .tsv files in the current directory
+tsv_files = glob.glob('*.tsv')
+
+# Iterate through each tsv file and convert it to an Excel worksheet
+for index, filename in enumerate(tsv_files):
+    # Read the tsv file into a Pandas DataFrame
+    dataframe = pd.read_csv(filename, sep='\t', header=None, on_bad_lines='skip')
+
+    # Create a new Worksheet in the Workbook
+    wb.create_sheet(title=filename)
+
+    # Get the new Worksheet object
+    ws = wb[filename]
+
+    # Convert DataFrame to a list of lists
+    rows = [list(row) for row in dataframe.values]
+
+    # Append the list of lists to the Worksheet
+    for row in rows:
+        ws.append(row)
+
+# Save and close the Workbook
+wb.save('SEEES search request.xlsx')
+wb.close()
