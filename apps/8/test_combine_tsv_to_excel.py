90707dbbcd1fbbeeb51c4d9327d872426a38c187
David Fulmer <dfulmer@umich.edu>
2024-09-16 11:13:14 -0400

    fix: modifying the apps/8 test file to get it to work

--- apps/8/test_combine_tsv_to_excel.py
+++ apps/8/test_combine_tsv_to_excel.py
@@ -1,38 +1,65 @@
 import os
 import pytest
 from combine_tsv_to_excel import combine_tsv_to_excel
+import pandas as pd
+
+# The first two are just generic samples to show the tmp_path format
+def test_create_file(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    p = d / "hello.txt"
+    p.write_text("CONTENT", encoding="utf-8")
+    assert p.read_text(encoding="utf-8") == "CONTENT"
+    assert len(list(tmp_path.iterdir())) == 1
+    #assert 0
+
+def test_create_file2(tmp_path):
+    #d = tmp_path / "sub"
+    #d.mkdir()
+    q = tmp_path / "hello2.txt"
+    q.write_text("CONTENT", encoding="utf-8")
+    assert q.read_text(encoding="utf-8") == "CONTENT"
+    #assert len(list(tmp_path.iterdir())) == 1
+
 
 @pytest.fixture
-def create_dummy_tsv_files(tmpdir):
+def create_dummy_tsv_files(tmp_path_factory):
+    path = tmp_path_factory.mktemp("sub")
     # Create dummy .tsv files for testing
-    tsv1 = tmpdir.join("file1.tsv")
-    tsv1.write("Column1\tColumn2\nValue1\tValue2\n")
+    #tsv1 = tmp_path.join("file1.tsv")
+    tsv1 = path / "file1.tsv"
+    tsv1.write_text("Column1\tColumn2\nValue1\tValue2\n")
 
-    tsv2 = tmpdir.join("file2.tsv")
-    tsv2.write("Column1\tColumn2\nValue3\tValue4\n")
+    tsv2 = path / "file2.tsv"
+    tsv2.write_text("Column1\tColumn2\nValue3\tValue4\n")
 
-    return tsv1, tsv2
+    return tsv1, tsv2, path
+
+@pytest.fixture
+def create_totally_empty_folder(tmp_path_factory):
+    pathb = tmp_path_factory.mktemp("subb")
+    return pathb
 
 def test_combine_tsv_to_excel(create_dummy_tsv_files):
-    tsv1, tsv2 = create_dummy_tsv_files
-    os.chdir(tmpdir)  # Change to the temporary directory
+    tsv1, tsv2, patha = create_dummy_tsv_files
+    os.chdir(patha)  # Change to the temporary directory
 
     combine_tsv_to_excel()
 
     assert os.path.exists("SEEES_search_request.xlsx")
 
-def test_no_tsv_files():
-    with pytest.raises(SystemExit):
-        combine_tsv_to_excel()
+def test_no_tsv_files(create_totally_empty_folder):
+    path = create_totally_empty_folder
+    os.chdir(path)
+    combine_tsv_to_excel()
+    assert os.path.exists("SEEES_search_request.xlsx") == False
 
 def test_sheet_names(create_dummy_tsv_files):
-    tsv1, tsv2 = create_dummy_tsv_files
-    os.chdir(tmpdir)
+    tsv1, tsv2, patha = create_dummy_tsv_files
+    os.chdir(patha)
 
     combine_tsv_to_excel()
 
     excel_file = pd.ExcelFile("SEEES_search_request.xlsx")
     sheet_names = excel_file.sheet_names
     assert sheet_names == ["file1", "file2"]
-
-# Add more tests as needed
